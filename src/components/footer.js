c2b950aeff835dfa86b2094ab6e1cad7954eeaf5
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-11-03 12:43:38 -0800

    create app footer and navigation

---
+++ src/components/footer.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMusic,
+  faSearch,
+  faEllipsisH
+} from "@fortawesome/free-solid-svg-icons";
+import styles from "../styles/footer.module.css";
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    <nav>
+      <ul className={styles.navUl}>
+        <li>
+          <NavLink
+            exact
+            to="/"
+            className={styles.navLink}
+            activeClassName={styles.active}
+          >
+            <div className={styles.navWrap}>
+              <FontAwesomeIcon icon={faMusic} />
+              Library
+            </div>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="search"
+            className={styles.navLink}
+            activeClassName={styles.active}
+          >
+            <div className={styles.navWrap}>
+              <FontAwesomeIcon icon={faSearch} />
+              Search
+            </div>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="more"
+            className={styles.navLink}
+            activeClassName={styles.active}
+          >
+            <div className={styles.navWrap}>
+              <FontAwesomeIcon icon={faEllipsisH} />
+              More
+            </div>
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  </footer>
+);
+
+export default Footer;
