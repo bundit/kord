20df00fb6d1579e258f6c028c88102659935bd38
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-11-05 15:46:53 -0800

    add header and footer to App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from "./components/header";
+import Footer from "./components/footer";
+// import Library from "./components/library";
+
+const App = () => (
+  <BrowserRouter>
+    <Route path="/" component={Header} />
+    <main className="content">
+      {/* <Route exact path="/" component={Library} /> */}
+    </main>
+    <Route path="/" component={Footer} />
+  </BrowserRouter>
+);
 
 export default App;
