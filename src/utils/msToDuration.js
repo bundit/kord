7cb7beb7bfe56d0bae717c9377ea56cc1ef7f928
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-11-11 15:11:54 -0800

    create util function - convert milliseconds

---
+++ src/utils/msToDuration.js
@@ -0,0 +1,15 @@
+export default function msToDuration(duration) {
+  console.log(duration);
+  const base = 10;
+
+  let seconds = parseInt((duration / 1000) % 60, base);
+  const minutes = parseInt((duration / (1000 * 60)) % 60, base);
+  const hours = parseInt((duration / (1000 * 60 * 60)) % 24, base);
+
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  if (hours >= 1) {
+    return `${hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds}`;
+  }
+  return `${minutes}:${seconds}`;
+}
