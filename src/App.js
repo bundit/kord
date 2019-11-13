af1ca72ef551fdb62498fb8409769c34797b1e85
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-11-13 12:55:44 -0800

    update component routes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import Header from "./components/layout/header";
 import Footer from "./components/layout/footer";
-import Library from "./components/pages/library";
+import Player from "./components/player";
+import Library from "./components/library-page";
+import Search from "./components/search-page";
 
 const App = () => (
   <BrowserRouter>
     <Route path="/" component={Header} />
     <main className="content">
       <Route exact path="/" component={Library} />
+      <Route exact path="/search" component={Search} />
     </main>
+    <Route path="/" component={Player} />
     <Route path="/" component={Footer} />
   </BrowserRouter>
 );
