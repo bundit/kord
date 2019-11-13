95b3ba0823813459457871119733da401b334a42
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-11-13 12:48:34 -0800

    create search page UI

---
+++ src/components/search-page.js
@@ -0,0 +1,144 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+import {
+  searchSouncloudTracks,
+  searchSoundcloudArtists,
+  loadMoreResults
+} from "../redux/actions/searchActions";
+import SearchBar from "./search-bar";
+import TrackItem from "./track-item";
+import styles from "../styles/library.module.css";
+
+// For restoring scroll position when component is unmounted
+let searchScrollPosition = null;
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleAddToLibrary = this.handleAddToLibrary.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+  componentDidMount() {
+    if (searchScrollPosition) {
+      document.querySelector("html").scrollTop = searchScrollPosition;
+    }
+  }
+
+  componentWillUnmount() {
+    searchScrollPosition = document.querySelector("html").scrollTop;
+  }
+
+  handleChange(event) {
+    const { setSearchQuery } = this.props;
+
+    setSearchQuery(event.target.value);
+  }
+
+  async handleSubmit(event) {
+    const { query, searchScTracks, searchScArtists } = this.props;
+
+    searchScTracks(query);
+    searchScArtists(query);
+
+    event.preventDefault();
+  }
+
+  handleAddToLibrary(track) {
+    const { importSong } = this.props;
+
+    importSong(track);
+  }
+
+  handleReset() {
+    const { resetQuery } = this.props;
+    resetQuery();
+  }
+
+  render() {
+    const { query, results, loadMoreTracks } = this.props;
+    const { handleChange, handleSubmit, handleReset } = this;
+    return (
+      <>
+        <SearchBar
+          placeholder="Search for Music"
+          query={query}
+          onChange={handleChange}
+          onSubmit={handleSubmit}
+          onReset={handleReset}
+        />
+        <div className={styles.libraryWrapper}>
+          {results &&
+            results.map(track => (
+              <TrackItem
+                search
+                key={track.id}
+                title={track.title}
+                artist={track.artist.name}
+                id={track.id}
+                img={track.img}
+                addToLibrary={() => this.handleAddToLibrary(track)}
+                ms={track.duration}
+              />
+            ))}
+          {!!results.length && (
+            <button type="button" onClick={loadMoreTracks}>
+              Load
+            </button>
+          )}
+        </div>
+      </>
+    );
+  }
+}
+
+Search.propTypes = {
+  query: PropTypes.string.isRequired,
+  results: PropTypes.arrayOf(PropTypes.object).isRequired,
+  setSearchQuery: PropTypes.func.isRequired,
+  importSong: PropTypes.func.isRequired,
+  loadMoreTracks: PropTypes.func.isRequired,
+  searchScTracks: PropTypes.func.isRequired,
+  searchScArtists: PropTypes.func.isRequired,
+  resetQuery: PropTypes.func.isRequired
+};
+
+const mapStateToProps = ({ search }) => ({
+  query: search.query,
+  results: search.results,
+  history: search.history
+});
+
+const mapDispatchToProps = dispatch => ({
+  searchScTracks: query => {
+    dispatch(searchSouncloudTracks(query));
+  },
+  searchScArtists: query => {
+    dispatch(searchSoundcloudArtists(query));
+  },
+  loadMoreTracks: () => dispatch(loadMoreResults()),
+  setSearchQuery: query =>
+    dispatch({
+      type: "SET_SEARCH_QUERY",
+      query
+    }),
+  importSong: track =>
+    dispatch({
+      type: "IMPORT_SONG",
+      payload: track
+    }),
+  resetQuery: () =>
+    dispatch({
+      type: "RESET_SEARCH_QUERY"
+    })
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search);
