9c82fbd16dfeb6d48a3aac68f753c49cbc956e71
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-11-13 10:23:07 -0800

    add more redux action types

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -1,6 +1,8 @@
-// Music Reducer
+// Library Reducer
 export const IMPORT_SC_LIKES = "IMPORT_SC";
-export const IMPORT_SC_SONG = "IMPORT_SONG";
+export const IMPORT_SONG = "IMPORT_SONG";
+export const SET_LIB_QUERY = "SET_LIB_QUERY";
+export const RESET_LIB_QUERY = "RESET_LIB_QUERY";
 
 // User Reducer
 export const SET_SC_USER = "SET_SC_USER";
@@ -9,8 +11,8 @@ export const SET_SC_USER = "SET_SC_USER";
 export const PLAY = "PLAY";
 export const PAUSE = "PAUSE";
 export const SEEK = "SEEK";
-export const NEXT = "NEXT";
-export const PREV = "PREV";
+export const NEXT_TRACK = "NEXT_TRACK";
+export const PREV_TRACK = "PREV_TRACK";
 export const ADD_TO_QUEUE = "ADD_TO_QUEUE";
 export const REMOVE_FROM_QUEUE = "REMOVE_FROM_QUEUE";
 export const PLAY_FROM_QUEUE = "PLAY_FROM_QUEUE";
@@ -18,4 +20,8 @@ export const SET_QUEUE = "SET_QUEUE";
 
 // Search Reducer
 export const SEARCH = "SEARCH";
+export const LOAD_MORE_RESULTS = "LOAD_MORE_RESULTS";
+export const SET_SEARCH_QUERY = "SET_SEARCH_QUERY";
+export const RESET_SEARCH_QUERY = "RESET_SEARCH_QUERY";
+export const SET_NEXT_HREF = "SET_NEXT_HREF";
 export const ADD_TO_SEARCH_HISTORY = "ADD_TO_SEARCH_HISTORY";
