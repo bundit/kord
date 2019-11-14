ca5edceefdb3226343e1542a9e54b8f84e289dfe
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-11-14 12:15:23 -0800

    add listener for playing track, hook for disabling add

--- src/components/track-item.js
+++ src/components/track-item.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faEllipsisH } from "@fortawesome/free-solid-svg-icons";
@@ -8,30 +8,58 @@ import msToDuration from "../utils/msToDuration";
 import styles from "../styles/library.module.css";
 import placeholderImg from "../assets/placeholder.png";
 
-const TrackItem = ({ title, img, artist, search, addToLibrary, ms }) => (
-  <div className={styles.trackWrapper}>
-    <img src={img || placeholderImg} alt="track" />
-    <div className={styles.titleWrapper}>
-      <div>
-        <strong>{truncateString(title, 38)}</strong>
+const TrackItem = ({
+  title,
+  img,
+  artist,
+  search,
+  addToLibrary,
+  ms,
+  handlePlay
+}) => {
+  const [disable, setDisable] = useState(false);
+  const handleDisable = () => {
+    setDisable(true);
+  };
+
+  return (
+    <div
+      className={styles.trackWrapper}
+      onClick={handlePlay}
+      role="button"
+      tabIndex="0"
+      onKeyPress={handlePlay}
+    >
+      <img src={img || placeholderImg} alt="track" />
+      <div className={styles.titleWrapper}>
+        <div>
+          <strong>{truncateString(title, 38)}</strong>
+        </div>
+        <div>{truncateString(artist, 38)}</div>
       </div>
-      <div>{truncateString(artist, 38)}</div>
+      <div className={styles.trackRightControls}>
+        <div className={styles.duration}>{msToDuration(ms)}</div>
+        {search && (
+          <button
+            type="button"
+            onClick={(event, track) => {
+              addToLibrary(event, track);
+              handleDisable();
+            }}
+            disabled={disable}
+          >
+            <FontAwesomeIcon icon={faPlus} />
+          </button>
+        )}
+        {!search && (
+          <button type="button">
+            <FontAwesomeIcon icon={faEllipsisH} />
+          </button>
+        )}
+      </div>
     </div>
-    <div className={styles.trackRightControls}>
-      <div className={styles.duration}>{msToDuration(ms)}</div>
-      {search && (
-        <button type="button" onClick={addToLibrary}>
-          <FontAwesomeIcon icon={faPlus} />
-        </button>
-      )}
-      {!search && (
-        <button type="button">
-          <FontAwesomeIcon icon={faEllipsisH} />
-        </button>
-      )}
-    </div>
-  </div>
-);
+  );
+};
 
 TrackItem.propTypes = {
   title: PropTypes.string.isRequired,
@@ -39,7 +67,8 @@ TrackItem.propTypes = {
   img: PropTypes.string,
   search: PropTypes.bool,
   addToLibrary: PropTypes.func,
-  ms: PropTypes.number.isRequired
+  ms: PropTypes.number.isRequired,
+  handlePlay: PropTypes.func.isRequired
 };
 
 TrackItem.defaultProps = {
