3408dbb9a91ceea5a02d1a7044e98953aac8fed5
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-11-17 11:46:19 -0800

    forcecheck visible components on componentdidupdate / add proptypes

--- src/components/track-list.js
+++ src/components/track-list.js
@@ -1,9 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { forceCheck } from "react-lazyload";
 
 import TrackItem from "./track-item";
 import styles from "../styles/library.module.css";
 
 class TrackList extends React.Component {
+  componentDidUpdate(prevProps) {
+    const { library: currentLibrary } = this.props;
+    const { library: prevLibrary } = prevProps;
+
+    if (currentLibrary !== prevLibrary) {
+      forceCheck();
+    }
+  }
+
   render() {
     const { library, handlePlay } = this.props;
 
@@ -25,5 +36,15 @@ class TrackList extends React.Component {
     );
   }
 }
+
+TrackList.propTypes = {
+  library: PropTypes.arrayOf(PropTypes.object),
+  handlePlay: PropTypes.func
+};
+
+TrackList.defaultProps = {
+  library: [],
+  handlePlay: () => {}
+};
 
 export default TrackList;
