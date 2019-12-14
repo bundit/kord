36d608324915757dc37ca1698fd54f0564ba2789
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-12-13 17:29:10 -0800

    fix jsdoc

--- src/utils/compareArtists.js
+++ src/utils/compareArtists.js
@@ -1,11 +1,11 @@
 /**
- * compareSongs - Compares two artists in alphabetic order
+ * compareArtists - Compares two artists in alphabetic order
  * Compares by artist name
  *
- * @param  {object} song1
- * @param  {object} song2
+ * @param  {object} artist1
+ * @param  {object} artist2
  * @return {number}
- * Returns > 0 if song1 comes first, < 0 if song2 comes first and 0 if they are the same
+ * Returns > 0 if artist1 comes first, < 0 if artist2 comes first and 0 if they are the same
  */
 export default function compareArtists(artist1, artist2) {
   const artistCompare = artist1.name
