cd1df050ce88810d850f904ae049c3058a1be162
Bundit Hongmanee <bundit.j.h@gmail.com>
2020-02-27 14:07:05 -0800

    Add server to server client build

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
+
+  // Catch all send client build from react-app
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else if (process.env.NODE_ENV === "development") {
+  // Serve react-app develment server in development
+  app.get("*", (req, res) => {
+    res.redirect("http://localhost:3000");
+  });
+}
+
+const port = process.env.PORT || 8888;
+app.listen(port, () => console.log(`Listening on port ${port}.`));
