df586226be9cd3938f56d433ef4f1cdf6d39b5d6
Bundit Hongmanee <bundit.j.h@gmail.com>
2020-03-13 21:24:45 -0700

    Create token refresh route

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
 const spotifyAuthRoutes = require("./spotify-auth-routes");
 // const mixcloudAuthRoutes = require("./mixcloud-auth-routes");
@@ -8,5 +9,34 @@ router.use(passport.initialize());
 
 router.use("/spotify", spotifyAuthRoutes);
 // router.use("/mixcloud", mixcloudAuthRoutes);
+
+router.get("/token", (req, res, next) => {
+  passport.authenticate("jwt", (err, user, info) => {
+    if (err) {
+      req.logout();
+      return res.send(err);
+    }
+
+    if (!user) {
+      req.logout();
+      return res.send("user is not logged in");
+    }
+
+    const newToken = jwt.sign(
+      {
+        username: user.email,
+        expires: Date.now() + parseInt(process.env.JWT_TOKEN_EXPIRE, 10)
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.cookie("kordUser", newToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production"
+    });
+
+    return res.send("OK");
+  })(req, res, next);
+});
 
 module.exports = router;
