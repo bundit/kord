6e8e973805479946dda7a3ba5016a9f37c047c78
Bundit Hongmanee <bundit.j.h@gmail.com>
2020-03-15 17:58:28 -0700

    Separate sign in/up logic to service

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,62 +2,11 @@ const passport = require("passport");
 const SpotifyStrategy = require("passport-spotify").Strategy;
 const JWTStrategy = require("passport-jwt").Strategy;
 
-const db = require("./database-setup");
+const AuthService = require("../services/auth");
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
-
-async function checkIfUserExists(client, email) {
-  const searchQuery = {
-    text: `SELECT *
-             FROM users
-             WHERE email=$1`,
-    values: [email]
-  };
-
-  const { rows: userSearchRows } = await client.query(searchQuery);
-
-  return userSearchRows.length && userSearchRows[0];
-}
-
-async function insertNewUser(client, email) {
-  const userInsertQuery = {
-    text: `INSERT INTO users(email, created_at)
-           VALUES
-           (
-              $1,
-              now()
-           )
-           RETURNING *;`,
-    values: [email]
-  };
-  const { rows: newUserRows } = await client.query(userInsertQuery);
-
-  return newUserRows[0];
-}
-
-async function insertUserProfile(client, user, provider) {
-  const { kordUserID, providerID, accessToken, refreshToken, images } = user;
-
-  const profileInsertQuery = {
-    text: `INSERT INTO user_profiles(user_id, oauth_provider, provider_id, access_token, refresh_token, images)
-             VALUES (
-               $1, $2, $3, $4, $5, $6
-             )
-           ON CONFLICT DO NOTHING;`,
-    values: [
-      kordUserID,
-      provider,
-      providerID,
-      accessToken,
-      refreshToken,
-      images
-    ]
-  };
-
-  await client.query(profileInsertQuery);
-}
 
 passport.use(
   new SpotifyStrategy(
@@ -67,31 +16,7 @@ passport.use(
       callbackURL: "http://localhost:8888/auth/spotify/callback"
     },
     (accessToken, refreshToken, expiresIn, profile, done) => {
-      db.getClient(async client => {
-        const {
-          id: spotifyUserID,
-          _json: { email, images }
-        } = profile;
-
-        let kordUser = await checkIfUserExists(client, email);
-
-        if (!kordUser) {
-          kordUser = await insertNewUser(client, email);
-        }
-
-        const newUserProfile = {
-          accessToken,
-          refreshToken,
-          kordUserID: kordUser.id,
-          providerID: spotifyUserID,
-          images
-        };
-
-        await insertUserProfile(client, newUserProfile, "spotify");
-
-        // Return the user profile to be serialized
-        done(null, kordUser);
-      });
+      AuthService.SignUpOrSignIn(profile, refreshToken, accessToken, done);
     }
   )
 );
---
+++ services/auth.js
@@ -0,0 +1,77 @@
+const db = require("../config/database-setup");
+
+// Returns false if does not exist, otherwise returns user row
+async function checkIfUserExists(client, email) {
+  const searchQuery = {
+    text: `SELECT *
+             FROM users
+             WHERE email=$1`,
+    values: [email]
+  };
+
+  const { rows: userSearchRows } = await client.query(searchQuery);
+
+  return userSearchRows.length && userSearchRows[0];
+}
+
+// Insert a user and return the new row
+async function insertNewUser(client, email) {
+  const userInsertQuery = {
+    text: `INSERT INTO users(email, created_at)
+           VALUES
+           (
+              $1,
+              now()
+           )
+           RETURNING *;`,
+    values: [email]
+  };
+  const { rows: newUserRows } = await client.query(userInsertQuery);
+
+  return newUserRows[0];
+}
+
+// Insert a new external user login profile
+async function insertUserProfile(client, user, provider) {
+  const { kordUserID, providerID, refreshToken, images } = user;
+
+  const profileInsertQuery = {
+    text: `INSERT INTO user_profiles(user_id, oauth_provider, provider_id, refresh_token, images)
+             VALUES (
+               $1, $2, $3, $4, $5
+             )
+           ON CONFLICT DO NOTHING;`,
+    values: [kordUserID, provider, providerID, refreshToken, images]
+  };
+
+  await client.query(profileInsertQuery);
+}
+
+module.exports = {
+  SignUpOrSignIn(profile, refreshToken, accessToken, done) {
+    db.getClient(async client => {
+      const {
+        id: providerID,
+        _json: { email, images },
+        provider
+      } = profile;
+
+      let kordUser = await checkIfUserExists(client, email);
+
+      if (!kordUser) {
+        kordUser = await insertNewUser(client, email);
+      }
+
+      const newUserProfile = {
+        kordUserID: kordUser.id,
+        refreshToken,
+        providerID,
+        images
+      };
+
+      await insertUserProfile(client, newUserProfile, provider);
+
+      done(null, { ...kordUser, accessToken });
+    });
+  }
+};
