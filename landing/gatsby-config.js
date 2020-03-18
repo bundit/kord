9b62cd4b79770935a7be971344400ef24346b195
Bundit Hongmanee <bundit.j.h@gmail.com>
2020-03-17 23:14:43 -0700

    Gatsby setup

---
+++ landing/gatsby-config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  siteMetadata: {
+    title: `kord: all your music, in one place.`,
+    description: `all the best musix`,
+    author: `@Bundit`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`
+        // icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /\.svg$/ // See below to configure properly
+        }
+      }
+    },
+    // `gatsby-plugin-transition-link`,
+    `gatsby-plugin-layout`
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+  proxy: {
+    prefix: "/auth",
+    url: "http://localhost:8888"
+  }
+};
