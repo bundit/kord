2e42c236a237359fff8b63d7754c3a0d9affdce9
Bundit Hongmanee <bundit.j.h@gmail.com>
2020-03-23 22:42:36 -0700

    Update header with desktop component and searchbar

--- client/src/components/layout/header.js
+++ client/src/components/layout/header.js
@@ -1,12 +1,34 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
+import {
+  faAngleLeft,
+  faAngleRight,
+  faCogs
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  searchSouncloudTracks,
+  searchSoundcloudArtists,
+  searchSpotify
+} from "../../redux/actions/searchActions";
+import PropTypes from "prop-types";
+import React from "react";
+import { connect } from "react-redux";
 
+import SearchBar from "../search-bar";
 import styles from "../../styles/header.module.css";
 
-function Header({ location }) {
+function Header({
+  history,
+  location,
+  libQuery,
+  setLibQuery,
+  resetQuery,
+  fetchQuery,
+  searchScTracks,
+  searchScArtists,
+  dispatchSearchSpotify,
+  setSearchQuery
+}) {
   const { pathname } = location;
   const baseUrls = ["Library", "Search", "More", ""];
 
@@ -22,24 +44,62 @@ function Header({ location }) {
   const prevRoute = pathname.slice(0, pathname.lastIndexOf("/"));
 
   return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.placeholder}>
-          {!baseUrls.includes(title) && (
-            <NavLink
-              style={{ color: "red", marginLeft: "0.3rem" }}
-              to={`${prevRoute}`}
-            >
-              <FontAwesomeIcon icon={faAngleLeft} size="3x" />
-            </NavLink>
-          )}
+    <>
+      <header className={styles.header}>
+        <div className={styles.mobileContainer}>
+          <div className={styles.placeholder}>
+            {!baseUrls.includes(title) && (
+              <NavLink
+                style={{ color: "red", marginLeft: "0.3rem" }}
+                to={`${prevRoute}`}
+              >
+                <FontAwesomeIcon icon={faAngleLeft} size="3x" />
+              </NavLink>
+            )}
+          </div>
+
+          <h1>{title}</h1>
+
+          <div className={styles.placeholder} />
         </div>
+      </header>
 
-        <h1>{title}</h1>
-
-        <div className={styles.placeholder} />
-      </div>
-    </header>
+      {/* Desktop Header */}
+      <header className={styles.deskHeader}>
+        <div className={styles.deskHeaderContainer}>
+          <div className={styles.navGroup}>
+            <button type="button" onClick={() => history.goBack()}>
+              <FontAwesomeIcon size="lg" icon={faAngleLeft} />
+            </button>
+            <button
+              disabed={window.history.next}
+              type="button"
+              onClick={() => history.goForward()}
+            >
+              <FontAwesomeIcon size="lg" icon={faAngleRight} />
+            </button>
+          </div>
+          <SearchBar
+            placeholder="Search for Music"
+            query={libQuery}
+            onChange={e => setLibQuery(e.target.value)}
+            // onSubmit={handleSubmit}
+            onReset={resetQuery}
+          />
+          <div className={styles.settingIconContainer}>
+            <button type="button" onClick={() => history.push("/settings")}>
+              <FontAwesomeIcon size="3x" icon={faCogs} />
+            </button>
+          </div>
+        </div>
+        <div className={styles.titleHeader}>
+          <div>
+            <h1>{title}</h1>
+            <span>No fluff. Just your music.</span>
+          </div>
+        </div>
+      </header>
+    </>
   );
 }
 
@@ -49,4 +109,35 @@ Header.propTypes = {
   }).isRequired
 };
 
-export default Header;
+const mapStateToProps = state => ({
+  libQuery: state.music.query,
+  fetchQuery: state.search.query
+});
+
+const mapDispatchToProps = dispatch => ({
+  setLibQuery: query =>
+    dispatch({
+      type: "SET_LIB_QUERY",
+      payload: query
+    }),
+  resetQuery: () => dispatch({ type: "RESET_LIB_QUERY" }),
+  searchScTracks: query => {
+    dispatch(searchSouncloudTracks(query));
+  },
+  searchScArtists: query => {
+    dispatch(searchSoundcloudArtists(query));
+  },
+  dispatchSearchSpotify: (query, scope, token) => {
+    dispatch(searchSpotify(query, scope, token));
+  },
+  setSearchQuery: query =>
+    dispatch({
+      type: "SET_SEARCH_QUERY",
+      query
+    })
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
