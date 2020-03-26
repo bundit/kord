3eac2d523e24a7a9ae4a64c042fb925a30dcb322
Bundit Hongmanee <bundit.j.h@gmail.com>
2020-03-25 21:18:19 -0700

    Add desktop buttons and desktop footer

--- client/src/components/minified-player.js
+++ client/src/components/minified-player.js
@@ -1,19 +1,26 @@
-import React from "react";
-import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faAngleUp,
+  faPlay,
   faPlayCircle,
   faPauseCircle
 } from "@fortawesome/free-solid-svg-icons";
+import PropTypes from "prop-types";
+import React from "react";
 
-import truncateString from "../utils/truncateString";
+import { ReactComponent as PlayIcon } from "../assets/play-button.svg";
+import { ReactComponent as BackwardIcon } from "../assets/backward.svg";
+import { ReactComponent as ForwardIcon } from "../assets/forward.svg";
+
 import styles from "../styles/player.module.css";
+import truncateString from "../utils/truncateString";
+import placeholderImg from "../assets/placeholder.png";
 
 const MinifiedPlayer = ({
   current: {
     title,
-    artist: { name: artistName }
+    artist: { name: artistName },
+    img
   },
   handleToggleExpand,
   handlePlayPause,
@@ -26,19 +33,46 @@ const MinifiedPlayer = ({
     role="button"
     onClick={handleToggleExpand}
   >
-    <button type="button" onClick={handleToggleExpand}>
+    <button
+      type="button"
+      onClick={handleToggleExpand}
+      className={styles.miniPlayerExpandButton}
+    >
       <FontAwesomeIcon icon={faAngleUp} />
     </button>
+    <div className={styles.miniPlayerImageWrap}>
+      <img
+        src={img ? img.replace("large.jpg", "t500x500.jpg") : placeholderImg}
+        alt="album-art"
+      />
+    </div>
     <div className={styles.titleWrapper}>
       <div>
         <strong>{truncateString(title, 38)}</strong>
       </div>
       <div>{truncateString(artistName, 38)}</div>
     </div>
-    <div>
-      <button type="button" onClick={handlePlayPause}>
-        <FontAwesomeIcon icon={isPlaying ? faPauseCircle : faPlayCircle} />
+    <button
+      type="button"
+      onClick={handlePlayPause}
+      className={styles.miniPlayerPlayButton}
+    >
+      <FontAwesomeIcon icon={isPlaying ? faPauseCircle : faPlay} />
+    </button>
+    <div className={styles.desktopButtonGroup}>
+      <button type="button" className={styles.backwardButton}>
+        <BackwardIcon />
       </button>
+      <button type="button" className={styles.desktopPlayButton}>
+        <PlayIcon />
+      </button>
+      <button type="button" className={styles.forwardButton}>
+        <ForwardIcon />
+      </button>
+    </div>
+
+    <div>
+      <input type="range" className={styles.slider} />
     </div>
   </div>
 );
