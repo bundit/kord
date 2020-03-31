e9fc0656f06d78d37a01ad30c0f29ea11f84e456
Bundit Hongmanee <bundit.j.h@gmail.com>
2020-03-30 23:27:32 -0700

    Update to only show connected playlists

--- client/src/components/playlist-list.js
+++ client/src/components/playlist-list.js
@@ -10,12 +10,14 @@ import styles from "../styles/library.module.css";
 const ListOfPlaylists = ({ playlists, toggleNewPlaylistForm }) => {
   const allPlaylists = flattenPlaylistObject(playlists);
 
-  const playlistComponents = allPlaylists.map(playlist => (
-    <PlaylistItem
-      key={`${playlist.source} ${playlist.title} ${playlist.id}`}
-      title={playlist.title}
-    />
-  ));
+  const playlistComponents = allPlaylists
+    .filter(playlist => playlist.isConnected)
+    .map(playlist => (
+      <PlaylistItem
+        key={`${playlist.source} ${playlist.title} ${playlist.id}`}
+        title={playlist.title}
+      />
+    ));
 
   return (
     <>
