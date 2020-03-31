70aed1a7d2bc77260c978551a63b3d8377f9e55b
Bundit Hongmanee <bundit.j.h@gmail.com>
2020-03-30 23:24:14 -0700

    Simplify saveState

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -16,20 +16,9 @@ store.subscribe(() => {
   const state = store.getState();
   // Store the library in localStorage
   saveState({
-    library: {
-      songs: state.library.songs,
-      artists: state.library.artists,
-      genres: state.library.genres,
-      playlists: state.library.playlists
-    },
+    library: state.library,
     user: {
-      soundcloud: {
-        key: state.user.soundcloud.key,
-        username: state.user.soundcloud.username
-      },
-      spotify: {
-        token: state.user.spotify.token
-      },
+      ...state.user,
       history: {
         library: ["/app/library"],
         search: [],
