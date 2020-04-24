e70ff499a2e18e316c32df9ec08438d2c311a55f
Bundit Hongmanee <bundit.j.h@gmail.com>
2020-04-23 22:57:36 -0700

    Remove lazyload, and dropdown menu related code

--- client/src/components/track-item.js
+++ client/src/components/track-item.js
@@ -1,13 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import {
   faSpotify,
   faSoundcloud,
   faYoutube
 } from "@fortawesome/free-brands-svg-icons";
-import LazyLoad from "react-lazyload";
 import PropTypes from "prop-types";
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 
 import { formatArtistName } from "../utils/formatArtistName";
 import { getImgUrl } from "../utils/getImgUrl";
@@ -16,166 +14,66 @@ import placeholderImg from "../assets/track-placeholder.jpg";
 import rippleEffect from "../utils/rippleEffect";
 import styles from "../styles/library.module.css";
 
-function addListenersToDocument(events, callback) {
-  events.forEach(e => document.addEventListener(e, callback));
-}
-
-function removeListenersOnDocument(events, callback) {
-  events.forEach(e => document.removeEventListener(e, callback));
-}
-
-function handleClickOutside(e, ref, events, callback, handler) {
-  // Check if the click was inside the target or if a scroll action was made
-  if (!ref.current.contains(e.target) || e.type === "scrollstart") {
-    callback();
-    removeListenersOnDocument(events, handler);
-  }
-}
-
-const TrackItem = ({
-  track,
-  search,
-  handlePlay,
-  addToLibrary,
-  isActive,
-  isPlaying
-}) => {
-  const { title, duration: ms, artist, genre, source } = track;
-  // Keep a reference to detect clicks outside of target area
-  const trackItemRef = useRef();
-  const [disable, setDisable] = useState(false);
-
-  const handleDisable = () => {
-    setDisable(true);
-  };
-
+const TrackItem = ({ track, handlePlay, isActive, isPlaying }) => {
+  const { title, duration: ms, artist, source } = track;
   const artistName = formatArtistName(artist);
 
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-  const events = ["mousedown", "touchstart", "scrollstart"];
-  function handler(e) {
-    handleClickOutside(
-      e,
-      trackItemRef,
-      events,
-      () => setIsDropdownOpen(false),
-      handler
-    );
+  function handlePlayTrack() {
+    handlePlay(track);
   }
-
-  const toggleDropdown = () => {
-    if (!isDropdownOpen) {
-      addListenersToDocument(events, handler);
-    } else {
-      removeListenersOnDocument(events, handler);
-    }
-
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-
-  // componentWillUnmount - cleanup listeners if there are any
-  useEffect(
-    () => () => {
-      removeListenersOnDocument(events, handler);
-    },
-    []
-  );
 
   if (track.source === "soundcloud" && !track.streamable) return null;
 
   return (
-    <div className={styles.relativeWrapper} ref={trackItemRef}>
-      <LazyLoad height={60} overflow={true} offset={200}>
-        <div
-          className={`${styles.trackWrapper} ${isActive && styles.playingNow}`}
-          onClick={e => {
-            rippleEffect(e);
-          }}
-          onDoubleClick={handlePlay}
-          role="button"
-          tabIndex="0"
-          onKeyPress={handlePlay}
-        >
-          <div className={styles.trackImageWrap}>
-            <img
-              className={styles.trackImage}
-              src={track.img ? getImgUrl(track, "sm") : placeholderImg}
-              alt="track"
+    <div
+      className={`${styles.trackWrapper} ${isActive && styles.playingNow}`}
+      onClick={e => {
+        rippleEffect(e);
+      }}
+      onDoubleClick={handlePlayTrack}
+      role="button"
+      tabIndex="0"
+      onKeyPress={handlePlayTrack}
+    >
+      <div className={styles.trackImageWrap}>
+        <img
+          className={styles.trackImage}
+          src={track.img ? getImgUrl(track, "sm") : placeholderImg}
+          alt="track"
+        />
+        {isActive && (
+          <div className={styles.overlay}>
+            <div className={`${styles.bar} ${!isPlaying && styles.paused}`} />
+            <div
+              className={`${styles.bar} ${styles.midBar} ${!isPlaying &&
+                styles.paused}`}
             />
-            {isActive && (
-              <div className={styles.overlay}>
-                <div
-                  className={`${styles.bar} ${!isPlaying && styles.paused}`}
-                />
-                <div
-                  className={`${styles.bar} ${styles.midBar} ${!isPlaying &&
-                    styles.paused}`}
-                />
-                <div
-                  className={`${styles.bar} ${!isPlaying && styles.paused}`}
-                />
-              </div>
-            )}
+            <div className={`${styles.bar} ${!isPlaying && styles.paused}`} />
           </div>
+        )}
+      </div>
 
-          <div className={styles.titleWrapper}>
-            <div>
-              <strong>{title}</strong>
-            </div>
-            <div className={styles.stackedArtistName}>{artistName}</div>
-          </div>
-          <div
-            className={styles.singleSource}
-            style={{ opacity: isActive && 1 }}
-          >
-            <FontAwesomeIcon
-              icon={
-                source === "spotify"
-                  ? faSpotify
-                  : source === "soundcloud"
-                  ? faSoundcloud
-                  : faYoutube
-              }
-              size="2x"
-            />
-          </div>
-          <div className={styles.trackRightControls}>
-            <div className={styles.duration}>{msToDuration(ms)}</div>
-            {search && (
-              <button
-                type="button"
-                onClick={event => {
-                  addToLibrary(event, track);
-                  handleDisable();
-                }}
-                disabled={disable}
-              >
-                <FontAwesomeIcon icon={faPlus} />
-              </button>
-            )}
-            {/* {!search && (
-              <button
-                type="button"
-                onClick={e => {
-              toggleDropdown();
-              e.stopPropagation();
-                }}
-                className={`${styles.dropDownButton} ${
-              isDropdownOpen ? styles.activeDropDown : ""
-                }`}
-              >
-                <span className={styles.ellipsisH}>
-              <FontAwesomeIcon icon={faEllipsisH} />
-                </span>
-                <span className={styles.ellipsisV}>
-              <FontAwesomeIcon size="2x" icon={faEllipsisV} />
-                </span>
-              </button>
-            )} */}
-          </div>
+      <div className={styles.titleWrapper}>
+        <div>
+          <strong>{title}</strong>
         </div>
-      </LazyLoad>
+        <div className={styles.stackedArtistName}>{artistName}</div>
+      </div>
+      <div className={styles.singleSource} style={{ opacity: isActive && 1 }}>
+        <FontAwesomeIcon
+          icon={
+            source === "spotify"
+              ? faSpotify
+              : source === "soundcloud"
+              ? faSoundcloud
+              : faYoutube
+          }
+          size="2x"
+        />
+      </div>
+      <div className={styles.trackRightControls}>
+        <div className={styles.duration}>{msToDuration(ms)}</div>
+      </div>
     </div>
   );
 };
@@ -197,8 +95,7 @@ TrackItem.propTypes = {
 };
 
 TrackItem.defaultProps = {
-  search: false,
-  addToLibrary: () => {}
+  search: false
 };
 
 export default TrackItem;
