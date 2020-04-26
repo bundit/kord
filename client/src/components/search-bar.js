3f43b2a4a0c62c9d0a3cc4c7bf134aa70bae98cc
Bundit Hongmanee <bundit.j.h@gmail.com>
2020-04-25 21:50:18 -0700

    Create search submit button and refactor styling

--- client/src/components/search-bar.js
+++ client/src/components/search-bar.js
@@ -1,25 +1,35 @@
-import React from "react";
 import PropTypes from "prop-types";
-import styles from "../styles/library.module.css";
+import React from "react";
+
+import styles from "../styles/searchForm.module.css";
 
 const SearchBar = ({ placeholder, query, onChange, onSubmit, onReset }) => (
-  <>
-    <form className={styles.searchForm} onSubmit={onSubmit} action="">
-      <input
-        className={styles.searchBar}
-        name="search"
-        type="search"
-        onChange={onChange}
-        placeholder={placeholder}
-        value={query}
-        // disable auto inputs
-        autoCapitalize="off"
-        autoComplete="off"
-        autoCorrect="off"
-        spellCheck="false"
-      />
-    </form>
-  </>
+  <form className={styles.searchForm} onSubmit={onSubmit} action="">
+    <input
+      className={`${styles.searchBar} ${query.length &&
+        styles.searchBarHasValue}`}
+      name="search"
+      type="search"
+      onChange={onChange}
+      placeholder={placeholder}
+      value={query}
+      // disable auto inputs
+      autoCapitalize="off"
+      autoComplete="off"
+      autoCorrect="off"
+      spellCheck="false"
+    />
+
+    <div className={styles.searchButtonContainer}>
+      <button
+        type="submit"
+        className={`${styles.submitSearchButton} ${query.length &&
+          styles.visibleButton}`}
+      >
+        Search
+      </button>
+    </div>
+  </form>
 );
 
 SearchBar.propTypes = {
