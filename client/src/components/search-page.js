12b1f20f8d25e8208b1eca379d2b181a31cccd56
Bundit Hongmanee <bundit.j.h@gmail.com>
2020-04-28 21:16:58 -0700

    Refactor search page component removing all dead code

--- client/src/components/search-page.js
+++ client/src/components/search-page.js
@@ -1,125 +1,27 @@
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import React from "react";
+import { Route } from "react-router-dom";
+import React, { useEffect } from "react";
 
-import { loadMoreResults } from "../redux/actions/soundcloudActions";
-import TrackList from "./track-list";
+import SearchResults from "./search-results";
+import styles from "../styles/library.module.css";
 
 // For restoring scroll position when component is unmounted
 let searchScrollPosition = null;
 
-class Search extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleAddToLibrary = this.handleAddToLibrary.bind(this);
-    this.handlePlayTrack = this.handlePlayTrack.bind(this);
-  }
-
-  componentDidMount() {
+const SearchPage = () => {
+  useEffect(() => {
     if (searchScrollPosition) {
       document.querySelector("html").scrollTop = searchScrollPosition;
     }
-  }
+    return () =>
+      (searchScrollPosition = document.querySelector("html").scrollTop);
+  }, []);
 
-  componentWillUnmount() {
-    searchScrollPosition = document.querySelector("html").scrollTop;
-  }
-
-  handleAddToLibrary(event, track) {
-    const { importSong } = this.props;
-
-    importSong(track);
-
-    // Prevent event bubbling to other handlers
-    event.stopPropagation();
-  }
-
-  handleReset() {
-    const { resetQuery } = this.props;
-    resetQuery();
-  }
-
-  handlePlayTrack(track) {
-    const { results, playTrack, setQueue } = this.props;
-    let index = 0;
-
-    // Find index of song clicked
-    while (results[index].id !== track.id && index < results.length) {
-      index += 1;
-    }
-
-    playTrack(track);
-
-    // Set queue to remaining songs
-    setQueue(results.slice(index));
-  }
-
-  render() {
-    const { results, loadMoreTracks, isPlaying, currentTrackID } = this.props;
-    const { handlePlayTrack, handleAddToLibrary } = this;
-
-    return (
-      <>
-        <TrackList
-          search
-          library={results}
-          handlePlay={handlePlayTrack}
-          addToLibrary={handleAddToLibrary}
-          loadMoreTracks={loadMoreTracks}
-          currentTrackID={currentTrackID}
-          isPlaying={isPlaying}
-        />
-      </>
-    );
-  }
-}
-
-Search.propTypes = {
-  results: PropTypes.arrayOf(PropTypes.object).isRequired,
-  // setSearchQuery: PropTypes.func.isRequired,
-  playTrack: PropTypes.func.isRequired,
-  importSong: PropTypes.func.isRequired,
-  loadMoreTracks: PropTypes.func.isRequired,
-  resetQuery: PropTypes.func.isRequired,
-  setQueue: PropTypes.func.isRequired,
-  isPlaying: PropTypes.bool.isRequired,
-  currentTrackID: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
-    .isRequired
+  return (
+    <div className={styles.searchPageWrapper}>
+      <Route exact path="/app/search" render={() => <h1>Search Page</h1>} />
+      <Route exact path="/app/search/:query" component={SearchResults} />
+    </div>
+  );
 };
 
-const mapStateToProps = state => ({
-  results: state.search.results,
-  history: state.search.history,
-  currentTrackID: state.player.currentTrack.id,
-  isPlaying: state.player.isPlaying,
-  spotifyAccessToken: state.user.spotify.token
-});
-
-const mapDispatchToProps = dispatch => ({
-  loadMoreTracks: () => dispatch(loadMoreResults()),
-  importSong: track =>
-    dispatch({
-      type: "IMPORT_SONG",
-      payload: track
-    }),
-  resetQuery: () =>
-    dispatch({
-      type: "RESET_SEARCH_QUERY"
-    }),
-  playTrack: track =>
-    dispatch({
-      type: "SET_TRACK",
-      payload: track
-    }),
-  setQueue: list =>
-    dispatch({
-      type: "SET_QUEUE",
-      payload: list
-    })
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Search);
+export default SearchPage;
