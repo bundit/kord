39c11474a09923472d431dff40d25256d95657ca
Bundit Hongmanee <bundit.j.h@gmail.com>
2020-05-20 16:10:54 -0700

    Edit 404 page

--- landing/src/pages/404.js
+++ landing/src/pages/404.js
@@ -1,11 +1,31 @@
 import React from "react";
 import SEO from "../components/seo";
+import styles from "../components/styles/landing.module.css";
 
 const NotFoundPage = () => (
   <>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className={styles.p1} style={{ height: "auto" }}>
+      <div className={styles.p1Content}>
+        <h1
+          className={styles.slogan}
+          data-aos="fade-down"
+          data-aos-easing="ease-in-out-back"
+          data-aos-duration="3000"
+          data-aos-delay="100"
+        >
+          404
+        </h1>
+        <h3
+          data-aos="fade-down"
+          data-aos-easing="ease-in-out-back"
+          data-aos-duration="1200"
+          data-aos-delay="1500"
+        >
+          Ruh roh, there&apos;s nothing here.
+        </h3>
+      </div>
+    </div>
   </>
 );
 
