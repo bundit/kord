4f811585d82d830b8d705a7d32628346669885ad
Bundit Hongmanee <bundit.j.h@gmail.com>
2020-06-16 18:37:17 -0700

    Create LinkAccount auth service

--- services/auth.js
+++ services/auth.js
@@ -1,3 +1,4 @@
+const jwtDecode = require("jwt-decode");
 const db = require("../config/database-setup");
 
 // Returns false if does not exist, otherwise returns user row
@@ -12,6 +13,21 @@ async function checkIfUserExists(client, email) {
   const { rows: userSearchRows } = await client.query(searchQuery);
 
   return userSearchRows.length && userSearchRows[0];
+}
+
+async function getUserById(client, kordId) {
+  const searchQuery = {
+    text: `SELECT *
+             FROM users
+             WHERE id=$1`,
+    values: [kordId]
+  };
+
+  const {
+    rows: [userRow]
+  } = await client.query(searchQuery);
+
+  return userRow;
 }
 
 // Insert a user and return the new row
@@ -64,6 +80,28 @@ module.exports = {
       if (!kordUser) {
         kordUser = await insertNewUser(client, email);
       }
+
+      const newUserProfile = {
+        kordUserID: kordUser.id,
+        refreshToken,
+        providerID,
+        images
+      };
+
+      await insertUserProfile(client, newUserProfile, provider);
+
+      done(null, { ...kordUser, accessToken });
+    });
+  },
+  LinkAccount(req, profile, refreshToken, accessToken, done) {
+    db.getClient(async client => {
+      const {
+        id: providerID,
+        _json: { images },
+        provider
+      } = profile;
+
+      const kordUser = jwtDecode(req.cookies.kordUser);
 
       const newUserProfile = {
         kordUserID: kordUser.id,
