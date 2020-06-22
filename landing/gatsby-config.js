0d94ec82babd05dd9295f3cbc599285105ce76b6
Bundit Hongmanee <bundit.j.h@gmail.com>
2020-06-21 17:41:00 -0700

    Edit manifest and add favicon, and add 192 and 512 logo sizes

--- landing/gatsby-config.js
+++ landing/gatsby-config.js
@@ -1,30 +1,30 @@
 module.exports = {
   siteMetadata: {
-    title: `kord: all your music, in one place.`,
-    description: `all the best musix`,
-    author: `@Bundit`
+    title: "kord",
+    description: "all your music, in one place.",
+    author: "@Bundit"
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-react-helmet",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `images`,
+        name: "images",
         path: `${__dirname}/src/assets`
       }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`
-        // icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        name: "Kord Player",
+        short_name: "Kord",
+        start_url: "/",
+        background_color: "#ffbb11",
+        theme_color: "#000000",
+        display: "standalone",
+        icon: "src/assets/favicon.png"
       }
     },
     {
@@ -35,10 +35,8 @@ module.exports = {
         }
       }
     },
-    `gatsby-plugin-layout`
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    "gatsby-plugin-layout",
+    "gatsby-plugin-offline"
   ],
   proxy: {
     prefix: "/auth",
