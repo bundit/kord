a3aa7b27b5c9c528f4fa49b7b8bbca9bac5ddb10
Bundit Hongmanee <bundit.j.h@gmail.com>
2021-03-06 19:18:15 -0800

    Refactor api route logic to api-controller

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,45 +1,18 @@
 const router = require("express").Router();
-const request = require("request");
-const KeyService = require("../services/keys");
+
 const {
   ensureAuthenticatedRequest
 } = require("../middleware/ensureAuthenticated");
+const {
+  getSoundcloudSearchResults,
+  getSuggestedAutocomplete
+} = require("../controllers/api-controller");
 
-// Authenticate all requests to /user
+// Authenticate all requests to /api
 router.use(ensureAuthenticatedRequest);
 
-router.get("/soundcloud", (req, res) => {
-  const {
-    query: { url }
-  } = req;
+router.get("/soundcloud", getSoundcloudSearchResults);
 
-  return request
-    .get(`${url}&client_id=${KeyService.soundcloudClientId}`)
-    .on("response", async response => {
-      const { statusCode } = response;
-
-      if (statusCode === 403 || statusCode === 401) {
-        await KeyService.refreshSoundcloudClientId();
-
-        return request
-          .get(`${url}&client_id=${KeyService.soundcloudClientId}`)
-          .pipe(res);
-      }
-
-      return response.pipe(res);
-    });
-});
-
-router.get("/autocomplete", (req, res) => {
-  const {
-    query: { q }
-  } = req;
-  const endpoint =
-    "http://suggestqueries.google.com/complete/search?client=chrome&ds=yt";
-
-  return request
-    .get(`${endpoint}&q=${encodeURIComponent(q)}`)
-    .on("response", response => response.pipe(res));
-});
+router.get("/autocomplete", getSuggestedAutocomplete);
 
 module.exports = router;
