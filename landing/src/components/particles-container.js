5dd97f4348511f94c92e4cb78e3ed91f7395ddf1
Bundit Hongmanee <bundit@Bundits-MBP.attlocal.net>
2023-07-18 22:31:52 -0700

    Fix particles

--- landing/src/components/particles-container.js
+++ landing/src/components/particles-container.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim";
 
 // window.addEventListener("load", event => {});
 let numParticles = 150;
@@ -14,6 +15,10 @@ if (process.env.NODE_ENV !== "development" && typeof window !== "undefined") {
 const particleParams = {
   particles: {
     number: {
+      density: {
+        enable: true
+        // area: 800
+      },
       value: numParticles
     },
     color: {
@@ -26,6 +31,18 @@ const particleParams = {
     //     color: "#fff"
     //   }
     // },
+    // line_linked: {
+    //   enable: true,
+    //   distance: 150,
+    //   color: "random",
+    //   opacity: 0.4,
+    //   width: 1,
+    //   triangles: {
+    //     enable: true,
+    //     color: "#ffffff",
+    //     opacity: 0.1
+    //   }
+    // },
     size: {
       value: 3,
       random: true
@@ -34,16 +51,16 @@ const particleParams = {
       value: 0.9,
       random: true
     },
-    line_linked: {
-      enable_auto: true,
-      // distance: 100,
-      color: "#fff",
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
       opacity: 0.7,
       width: 1.5
     },
     move: {
       enable: true,
-      speed: 1.2,
+      speed: 0.5,
       direction: "none",
       random: false,
       straight: false,
@@ -53,6 +70,9 @@ const particleParams = {
         enable: false,
         rotateX: 600,
         rotateY: 1200
+      },
+      outModes: {
+        default: "bounce"
       }
     }
   },
@@ -60,7 +80,7 @@ const particleParams = {
     events: {
       onhover: {
         enable: true,
-        mode: "grab"
+        mode: "link"
       },
       onclick: {
         enable: true,
@@ -69,6 +89,7 @@ const particleParams = {
     },
     modes: {
       grab: {
+        distance: 400,
         line_linked: {
           opacity: 0.7
         }
@@ -79,23 +100,34 @@ const particleParams = {
   }
 };
 
-const ParticlesContainer = () => (
-  <div
-    style={{
-      position: "fixed",
-      background:
-        "linear-gradient(0deg, rgba(255,187,17,1) 0%, rgba(255,200,66,1) 100%)",
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      zIndex: -1
-    }}
-    data-aos="zoom-out"
-    data-aos-duration="1500"
-  >
-    <Particles style={{ position: "absolute" }} params={particleParams} />
-  </div>
-);
+function ParticlesContainer() {
+  async function initParticles(engine) {
+    await loadSlim(engine);
+  }
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        background:
+          "linear-gradient(0deg, rgba(255,187,17,1) 0%, rgba(255,200,66,1) 100%)",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        zIndex: -1
+      }}
+      data-aos="zoom-out"
+      data-aos-duration="1500"
+    >
+      <Particles
+        id="tsparticles"
+        style={{ position: "absolute" }}
+        init={initParticles}
+        options={particleParams}
+      />
+    </div>
+  );
+}
 
 export default ParticlesContainer;
