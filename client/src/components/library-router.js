b3457da68ea203d4f401b34d87d41ba2c9ec7ad2
Bundit Hongmanee <bundit@Bundits-MBP.attlocal.net>
2023-07-21 23:05:44 -0700

    Temporary fix for pages not fetching tracklists

--- client/src/components/library-router.js
+++ client/src/components/library-router.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useParams } from "react-router-dom";
 
 import LibraryViewPage from "./library-view-page";
 import PlaylistPage from "./playlist-page";
 
 const LibraryRouter = () => {
+  // Workaround to force PlaylistPage remount after page change until better logic flow later
+  const { "*": urlPath } = useParams();
+
   return (
     <Routes>
       <Route path="/" element={<LibraryViewPage />} />
-      <Route path="playlists/:source/:id/:title" element={<PlaylistPage />} />
+      <Route
+        path="playlists/:source/:id/:title"
+        element={<PlaylistPage key={urlPath} />}
+      />
     </Routes>
   );
 };
--- client/src/components/search-router.js
+++ client/src/components/search-router.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useParams } from "react-router-dom";
 
 import ArtistPage from "./artist-page";
 import SearchHistoryPage from "./search-history-page";
 import SearchResultsPage from "./search-results-page";
 
 const SearchRouter = () => {
+  // Workaround to force ArtistPage remount after page change until better logic flow later
+  const { "*": urlPath } = useParams();
+
   return (
     <Routes>
       <Route path="/" element={<SearchHistoryPage />} />
       <Route path="/:query" element={<SearchResultsPage />} />
       <Route
         path="/artist/:source/:artistId/:artistName"
-        element={<ArtistPage />}
+        element={<ArtistPage key={urlPath} />}
       />
     </Routes>
   );
