b897072727980ec3b3d1b8ae371b447d67b6166d
bundit <bundit.dev@gmail.com>
2023-08-05 00:34:20 -0700

    Renaming ts files

--- middleware/ssl.ts
+++ middleware/ssl.ts
@@ -1,4 +1,6 @@
-module.exports = function forceSSL(req, res, next) {
+import { NextFunction, Request, Response } from "express";
+
+export = function forceSSL(req: Request, res: Response, next: NextFunction) {
   if (req.header("x-forwarded-proto") !== "https") {
     return res.redirect(`https://www.${req.header("host")}${req.url}`);
   }
